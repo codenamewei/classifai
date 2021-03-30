5cb5c44e574e2348edd15c72535353b6aaf7d78c
codenamewei <codenamewei@gmail.com>
2021-03-31 07:35:51 +0800

    :card_file_box: Primary key of Wasabi Table changed

--- classifai-core/src/main/java/ai/classifai/database/wasabis3/WasabiQuery.java
+++ classifai-core/src/main/java/ai/classifai/database/wasabis3/WasabiQuery.java
@@ -29,7 +29,7 @@ public class WasabiQuery
 {
     @Getter private static final String queue = "wasabi.queue";
 
-    @Getter private static final String createTable = "CREATE TABLE IF NOT EXISTS Wasabi (cloud_id VARCHAR(50), project_id UUID, access_key VARCHAR(200), secret_access_key VARCHAR(200), bucket VARCHAR(50), PRIMARY KEY (cloud_id, project_id))";
+    @Getter private static final String createTable = "CREATE TABLE IF NOT EXISTS Wasabi (cloud_id VARCHAR(50), project_id UUID, access_key VARCHAR(200), secret_access_key VARCHAR(200), bucket VARCHAR(50), PRIMARY KEY (project_id))";
 
     @Getter private static final String writeCredential = "INSERT INTO Wasabi VALUES (?, ?, ?, ?, ?)";
 
