08dcad3273d234fb08761d076009e44b405e3486
devenyantis <deven.yantis@certifai.ai>
2021-06-03 17:18:45 +0800

    :white_check_mark: Adding test

--- classifai-core/src/test/java/ai/classifai/action/ProjectImportTest.java
+++ classifai-core/src/test/java/ai/classifai/action/ProjectImportTest.java
@@ -4,6 +4,7 @@ import ai.classifai.TestHelper;
 import io.vertx.core.json.JsonObject;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -11,16 +12,32 @@ import java.io.IOException;
 import java.util.stream.Stream;
 
 public class ProjectImportTest {
+    @DisplayName("Check json keys success case")
     @ParameterizedTest
-    @MethodSource("jsonKeysSource")
-    void testCheckJsonKeys(JsonObject inputJsonObject) {
+    @MethodSource("testCheckJsonKeysSuccessSource")
+    void testCheckJsonKeysSuccess(JsonObject inputJsonObject) {
         boolean output = ProjectImport.checkJsonKeys(inputJsonObject);
         Assertions.assertTrue(output);
     }
 
-    static Stream<JsonObject> jsonKeysSource() throws IOException, ParseException {
+    static Stream<JsonObject> testCheckJsonKeysSuccessSource() throws IOException, ParseException {
         return Stream.of(
-                TestHelper.readJsonFile("src/test/resources/FirstProject.json")
+                TestHelper.readJsonFile("src/test/resources/SuccessCase.json")
+        );
+    }
+
+    @DisplayName("Check json keys fail case")
+    @ParameterizedTest
+    @MethodSource("testCheckJsonKeysFailSource")
+    void testCheckJsonKeysFail(JsonObject inputJsonObject) {
+        boolean output = ProjectImport.checkJsonKeys(inputJsonObject);
+        Assertions.assertFalse(output);
+    }
+
+    static Stream<JsonObject> testCheckJsonKeysFailSource() throws IOException, ParseException {
+        return Stream.of(
+                TestHelper.readJsonFile("src/test/resources/FailCaseIncorrectKeySpelling.json"),
+                TestHelper.readJsonFile("src/test/resources/FailCaseMissingKey.json")
         );
     }
 }
--- classifai-core/src/test/java/ai/classifai/util/data/FileHandlerTest.java
+++ classifai-core/src/test/java/ai/classifai/util/data/FileHandlerTest.java
@@ -1,15 +1,56 @@
 package ai.classifai.util.data;
 
 import ai.classifai.data.type.image.ImageFileType;
-import ai.classifai.util.data.FileHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class FileHandlerTest {
+    @ParameterizedTest
+    @MethodSource("testProcessFolderSuccessSource")
+    @DisplayName("Iterate folder success case")
+    void testProcessFolderSuccess(File rootPath, String[] extensionFormat, int testCorrectValue) {
+        List<File> outputList =  FileHandler.processFolder(rootPath, extensionFormat);
+        Assertions.assertEquals(outputList.size(), testCorrectValue);
+    }
+
+    static Stream<Object> testProcessFolderSuccessSource() {
+        String[] fileExtension = ImageFileType.getImageFileTypes();
+        return Stream.of(
+                Arguments.of(
+                        new File("src/test/resources/validImages"), fileExtension, 3
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProcessFolderFailSource")
+    @DisplayName("Iterate folder fail case")
+    void testProcessFolderFail(File rootPath, String[] extensionFormat, int testCorrectValue) {
+        List<File> outputList =  FileHandler.processFolder(rootPath, extensionFormat);
+        // Value should not be equal to number of files in the input rootpath
+        Assertions.assertNotEquals(outputList.size(), testCorrectValue);
+    }
+
+    static Stream<Object> testProcessFolderFailSource() {
+        String[] fileExtension = ImageFileType.getImageFileTypes();
+        return Stream.of(
+                Arguments.of(
+                        new File("src/test/resources/invalidImages"), fileExtension, 3
+                )
+        );
+    }
+
     @Test
     @DisplayName("Check supported file")
-    void testisFileSupported()
+    void testIsFileSupported()
     {
         String fileName = "image.jpg";
         boolean result = FileHandler.isFileSupported(fileName, ImageFileType.getImageFileTypes());
@@ -18,10 +59,11 @@ public class FileHandlerTest {
 
     @Test
     @DisplayName("Check supported file directory string")
-    void testisFileSupportedDirectoryString()
+    void testIsFileSupportedDirectoryString()
     {
         String fileName = "path/to/my/image.jpg";
         boolean result = FileHandler.isFileSupported(fileName, ImageFileType.getImageFileTypes());
         Assertions.assertTrue(result);
     }
+
 }
