62150ccca20fbb8512ace491702a2abf04285d04
YCCertifai <yinchuang96@gmail.com>
2021-06-17 14:31:27 +0800

    :lipstick: Add UI for database migration

--- classifai-core/src/main/java/ai/classifai/ui/component/DatabaseMigrationUi.java
+++ classifai-core/src/main/java/ai/classifai/ui/component/DatabaseMigrationUi.java
@@ -1,2 +1,29 @@
-package ai.classifai.ui.component;public class DatabaseMigrationUi {
+package ai.classifai.ui.component;
+
+import javax.swing.*;
+
+public class DatabaseMigrationUi
+{
+    JFrame mainFrame;
+    JPanel mainPanel;
+
+    public DatabaseMigrationUi()
+    {
+        mainFrame = new JFrame("Migration Options");
+        mainPanel = new JPanel();
+    }
+
+    public void run()
+    {
+        mainFrame.setVisible(true);
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        DatabaseMigrationUi ui = new DatabaseMigrationUi();
+
+    }
+
 }
